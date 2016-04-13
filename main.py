6d216811b2e5ccf4c12ed530cd7fdf80a59276d4
Author: Eugenio <eugeniomartinez92@gmail.com>
2016-04-12 19:53:42 -0500

    MEEEEEP

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import sys 
-from antlr4 import * 
-from funk import Funk
-from tabla import Tabla
-from gijackLexer import gijackLexer
-from gijackParser import gijackParser
- 
+#!/usr/bin/python
+
+import sys
+from parser.funk import Funk
+from parser.tabla import Tabla
+from parser.gijackLexer import gijackLexer
+from parser.gijackParser import gijackParser
+from antlr4 import *
+
 def main(argv):
     tabla = Tabla()
     inputfile = FileStream(argv[1])
@@ -13,13 +15,13 @@ def main(argv):
     parser = Funk(stream, tabla)
 
     try:
-        tree = parser.start()   
+        tree = parser.start()
     except Exception as e:
         print 'Hubo un error: ' + str(e)
 
     tabla.print_vars()
     tabla.print_funcion()
     tabla.print_constante()
- 
+
 if __name__ == '__main__':
     main(sys.argv)
